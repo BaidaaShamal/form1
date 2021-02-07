16572056bb896f39ac5369ddcf0b18da704961cc
BaidaaShamal <l3274918@gmail.com>
2021-02-07 10:09:45 +0300

    Add the possibility of deletion

---
+++ TableFrom/LoginForm.js
@@ -0,0 +1,13 @@
+
+function verifyPassword() {
+    var pw = document.getElementById("userPassword").value,
+        us =document.getElementById("userName").value;
+        
+        if((us =="") || (us.length < 8) || (!isNaN(us))){
+            document.getElementById("message").innerHTML="Fill the name please!"
+           } else if((pw == "") || (pw.length < 8) ||((pw.length > 20))) {  
+       document.getElementById("message2").innerHTML = "Fill the password please!";  
+       return false; }  else {  
+      window.location.replace('TableForm.html');  }  
+  }  
+  
